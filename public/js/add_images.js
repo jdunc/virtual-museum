0850dbd2b906872680b6b377af3d0ecbc89da145
Jordan Duncan <j.duncan@rochester.edu>
2017-01-03 17:13:45 -0600

    working on alter items page

--- public/js/add_images.js
+++ public/js/add_images.js
@@ -1,14 +1,18 @@
  $(document).ready(function(){
+   console.log('add images js here');
   $(".button-collapse").sideNav();
   $('select').material_select();
   $.get({
     url: '/items',
   })
   .then(function(results){
-    $(results).each(() =>{
+    console.log(results);
+    $(results).each(function(){
+      console.log(this.name);
       $('#selections').append(
-        `<option value=${results.id}>${results.name}</option>`
+        `<option value=${this.id}>${this.name}</option>`
       )
-    })
+    });
+    $('select').material_select();
   })
 }) //end on document ready
---
+++ public/js/alter_items.js
@@ -0,0 +1,115 @@
+$(document).ready(function(){
+  console.log('add images js here');
+ $(".button-collapse").sideNav();
+ $('select').material_select();
+ $.get({
+   url: '/items',
+ })
+ .then(function(results){
+   console.log(results);
+   $(results).each(function(){
+     console.log(this.name);
+     $('#selections').append(
+       `<option value=${this.id}>${this.name}</option>`
+     )
+   });
+   $('select').material_select();
+ });
+
+ $( "#selections" ).change(function() {
+  var selectedItemId = $('#selections option:selected')[0].value;
+  fillPage(selectedItemId);
+});
+
+}) //end on document ready
+
+function fillPage(item){
+  $.get({
+    url: `/items/${item}`,
+  })
+  .then(function(getData){
+    $('#alterForm').remove();
+    $('body').append(
+      `<div class="container" id="alterForm">
+        <br>
+      <div class="row">
+        <form class="col s12">
+          <div class="row">
+            <div class="input-field col s12">
+              <input placeholder="" id="name" type="text" class="validate" value="${getData.name}" >
+              <label for="name" class="active">Object Name</label>
+            </div>
+            <div class="input-field col s12">
+              <input id="type" type="text" value="${getData.type}">
+              <label for="type" class="active">Type</label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="input-field col s12">
+              <input id="culture" type="text" value="${getData.culture}">
+              <label for="culture" class="active">Culture</label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="input-field col s12">
+              <input id="collection" type="text" value="${getData.collection}">
+              <label for="collection" class="active">Collection</label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="input-field col s12">
+              <input id="location" type="text" value="${getData.location}" >
+              <label for="location" class="active">Location</label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="input-field col s12">
+              <input id="story" type="text" value="${getData.story}" >
+              <label for="story" class="active">Story</label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="input-field col s12">
+              <input id="description" type="text" value="${getData.description}" >
+              <label for="description" class="active">Description</label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="input-field col s12">
+              <input id="dimensions" type="text" value="${getData.dimensions}" >
+              <label for="dimensions" class="active">Dimensions</label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="input-field col s12">
+              <input id="provenance" type="text" value="${getData.provenance}" >
+              <label for="provenance" class="active">Provenance</label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="input-field col s12 ">
+              <input id="submit" type="submit" class="waves-effect waves-light btn"  >
+            </div>
+          </div>
+        </form>
+      </div>
+      </div>
+      `
+    )
+  });
+}
+
+
+// $('body').append(
+//   `<h4>Object Added</h4><br>
+//         <h5>Item Name</h5><a>${getData.name}</a>
+//         <h5>Type</h5><a>${getData.type}</a>
+//         <h5>Location</h5><a>${getData.culture}</a>
+//         <h5>Culture</h5><a>${getData.collection}</a>
+//         <h5>Collection</h5><a>${getData.location}</a>
+//         <h5>Story</h5><a>${getData.story}</a>
+//         <h5>Description</h5><a>${getData.description}</a>
+//         <h5>Dimensions</h5><a>${getData.dimensions}</a>
+//         <h5>Provenance</h5><a>${getData.provenance}</a>
+//   `
+// )
